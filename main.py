79d3a481d327a1eeaf25dc173d55e5e56d597cb1
Soccmai <114106608+Soccmai@users.noreply.github.com>
2024-09-19 13:01:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,113 @@
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+import json
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+               's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+               'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letter = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letter + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    input_password.delete(0, END)
+    input_password.insert(0, password)
+    pyperclip.copy(password)
+
+
+def save():
+    website = input_website.get()
+    email = input_email.get()
+    password = input_password.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            input_website.delete(0, END)
+            input_password.delete(0, END)
+
+
+def find_password():
+    website = input_website.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+            pyperclip.copy(password)
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+input_website = Entry(width=21)
+input_website.grid(column=0, row=1, columnspan=3)
+input_website.focus()
+search_button = Button(text="Search", width=20, command=find_password)
+search_button.grid(column=2, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+input_email = Entry(width=37)
+input_email.grid(column=1, row=2, columnspan=2)
+input_email.insert(0, "rcsokmai24@gmail.com")
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+input_password = Entry(width=21)
+input_password.grid(column=0, row=3, columnspan=3)
+generate_button = Button(text="Generate Password", width=20, command=generate_password)
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=5, columnspan=2)
+
+window.mainloop()
